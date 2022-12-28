9d369f76016788514670f87102d4b180b068c227
Снежана Мороз <sma310202@gmail.com@>
2022-12-28 23:45:06 +0700

    Переключения состояний

--- lab9.js
+++ lab9.js
@@ -4,3 +4,26 @@ function clickMe(button){
     else
         button.innerHTML = 'Щелчок';
 }
+function clickName(button){
+    if(button.innerHTML == 'Снежана')
+        button.innerHTML = 'Мороз';
+    else
+        button.innerHTML = 'Снежана';
+}
+function clickMe2(button){
+    if( button.innerHTML == 'Нажми меня'){
+        button.innerHTML = 'Ещё раз нажми';
+        button.style.color = 'DarkSlateBlue';
+    }
+    else if( button.innerHTML == 'Ещё раз нажми'){
+        button.innerHTML = 'Ещё';
+        button.style.color = 'Brown';}
+    else if( button.innerHTML == 'Ещё'){
+        button.innerHTML = 'Начать сначала';
+        button.style.color = 'Biege';}
+    else if( button.innerHTML == 'Начать сначала'){
+        button.innerHTML = 'Нажми меня';
+        button.style.color = 'Dark Brown';}
+    else
+        button.innerHTML = 'Нажми меня';
+}
