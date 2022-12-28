5f916e66557e8b1667437f26bf9e4276a542dc92
Снежана Мороз <sma310202@gmail.com@>
2022-12-28 23:47:09 +0700

    Вывод координат мыши

--- lab9.js
+++ lab9.js
@@ -23,7 +23,15 @@ function clickMe2(button){
         button.style.color = 'Biege';}
     else if( button.innerHTML == 'Начать сначала'){
         button.innerHTML = 'Нажми меня';
-        button.style.color = 'Dark Brown';}
+        button.style.color = 'Dark';}
     else
         button.innerHTML = 'Нажми меня';
 }
+function showCoords(event){
+    event.target.innerHTML = 'x = ' + event.offsetX + ', y = ' + event.offsetY + ' ' + 
+    (event.ctrlKey? '<b>Ctrl</b>': '<s>Ctrl</s>') + ' ' + (event.shiftKey? '<b>Shift</b>': '<s>Shift</s>') + ' ' + (event.altKey? '<b>Alt</b>': '<s>Alt</s>');
+}
+window.addEventListener('DOMContentLoaded', function() {
+    let coords = document.querySelector('#coords');
+    coords.addEventListener('mousemove', showCoords);
+});
