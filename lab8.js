43cd10692843e29a0a8c0e0e38d7da7524067240
Снежана Мороз <sma310202@gmail.com@>
2022-12-28 23:22:31 +0700

    Количество дней с даты рождения

--- lab8.js
+++ lab8.js
@@ -5,3 +5,12 @@ function showDate(params) {
 }
 
 window.addEventListener('load', showDate);
+function showDaysCount(params){
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    let result = document.getElementById('result');
+    daysCount = Math.ceil (daysCount);
+    result.innerHTML = 'С даты рождения прошло дней:' + daysCount;
+}
