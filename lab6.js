39d8cdba85b765d4fff806ac9f3cc0a122b71f79
Снежана Мороз <sma310202@gmail.com@>
2022-12-28 22:05:22 +0700

    Alert

--- lab6.js
+++ lab6.js
@@ -37,3 +37,30 @@ function setEnglish() {
     document.getElementById('ok-button').value = 'ok';
     document.getElementById('reset-button').value = 'reset';
 }
+function showAlert() {
+    let userName = document.getElementById('input-name').value;
+    let userSurname = document.getElementById('input-surname').value;
+    let userAge = document.getElementById('input-age').value;
+    alert('Привет,' + userName + ' ' + userSurname + '\n' + 'Ваш возраст:' + userAge);
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('user-data').style.backgroundColor = color;
+}
+function StudentInfo() {
+    alert('Шапка');
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('head').style.backgroundColor = color;
+}
+function FooterInfo() {
+    alert('Подвал');
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('footer').style.backgroundColor = color;
+}
